cbdea205d98a88f9270db8b91244944d9170e864
ocornoc <ocornoc@protonmail.com>
2021-02-19 11:25:44 -0800

    Move off dir into off.rs to clean up hierarchy

--- src/polytope/off.rs
+++ src/polytope/off.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::fs::read;
 use std::io::Result as IoResult;
 use std::path::Path;
 
@@ -252,7 +251,61 @@ pub fn from_src(src: String) -> PolytopeSerde {
 }
 
 pub fn from_path(fp: &Path) -> IoResult<PolytopeSerde> {
-    Ok(from_src(String::from_utf8(read(fp)?).unwrap()))
+    Ok(from_src(String::from_utf8(std::fs::read(fp)?).unwrap()))
+}
+
+const EL_NAMES: &[&str] = &["Vertices", "Edges", "Faces"];
+
+/// A set of options to be used when saving the OFF file.
+#[derive(Clone, Copy)]
+pub struct OFFOptions {
+    /// Whether the OFF file should have comments specifying each face type.
+    comments: bool,
+    /// Whether all unnecessary whitespace should be removed.
+    compress: bool,
+    /// Whether the file should be compatible with Stella (3D and 4D only).
+    stella_compat: bool,
+}
+
+impl Default for OFFOptions {
+    fn default() -> Self {
+        OFFOptions {
+            comments: true,
+            compress: false,
+            stella_compat: true,
+        }
+    }
+}
+
+pub fn to_src(p: Polytope, opt: OFFOptions) -> String {
+    let dim = p.rank();
+    let mut off = String::new();
+    let mut opt = opt;
+
+    // Stella compatibility only matters in 3D and 4D.
+    if !(dim == 3 || dim == 4) {
+        opt.stella_compat = false;
+    }
+
+    // Newline character.
+    let newline = if !opt.stella_compat && opt.compress {
+        " "
+    } else {
+        "\n"
+    };
+
+    // Writes header.
+    if dim != 3 {
+        off += &dim.to_string();
+    }
+    off += "OFF";
+    off += newline;
+
+    off
+}
+
+pub fn to_path(fp: &Path, p: Polytope, opt: OFFOptions) -> IoResult<()> {
+    std::fs::write(fp, to_src(p, opt))
 }
 
 #[cfg(test)]
