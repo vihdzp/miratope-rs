6b2cc55910646addfce9bd654a606279c2e28660
Zerocumulus <ultraroman01@gmail.com>
2021-04-07 11:35:08 -0500

    test lol

--- src/polytope/cd.rs
+++ src/polytope/cd.rs
@@ -131,6 +131,10 @@ fn multi_cd_parse(diagram: &str) -> Option<Vec<Graph<EdgeVal, EdgeVal, Undirecte
 }
 */
 
+struct test {
+    foo: i64,
+}
+
 /// Packages important information needed to interpret CDs
 struct Caret<'a> {
     diagram: Chars<'a>,
