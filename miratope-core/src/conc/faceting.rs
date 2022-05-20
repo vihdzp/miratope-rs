24e2cf07b53e0d98ef60f9b58be8dc5ee480caa8
H-A-M-G-E-R <98665992+H-A-M-G-E-R@users.noreply.github.com>
2022-05-19 19:41:05 -0500

    r
    r

--- miratope-core/src/conc/faceting.rs
+++ miratope-core/src/conc/faceting.rs
@@ -1456,22 +1456,23 @@ impl Concrete {
 
                     if save {
 	            	if r {
-				if fissary_status == "" {
-                                    output.push((poly.clone(), Some(
+			    if fissary_status == "" {
+                                output.push((poly.clone(), Some(
                                     if save_facets {
                                         format!("faceting {} -{}{}", faceting_idx, facets_fmt, fissary_status)
                                     } else {
                                         format!("faceting {}{}", faceting_idx, fissary_status)
-                                }
+                                    }
 				)));
 		        } else {
 			    output.push((poly.clone(), Some(
-                            if save_facets {
-                                format!("faceting {} -{}{}", faceting_idx, facets_fmt, fissary_status)
-                            } else {
-                                format!("faceting {}{}", faceting_idx, fissary_status)
+                                if save_facets {
+                                    format!("faceting {} -{}{}", faceting_idx, facets_fmt, fissary_status)
+                                } else {
+                                    format!("faceting {}{}", faceting_idx, fissary_status)
+			        }
+                            )));
 			}
-                        )));
                     }
 
                     if save_facets {
