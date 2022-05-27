4ec46a59af2b1c0046ed7dfc2f37cb9382425235
H-A-M-G-E-R <98665992+H-A-M-G-E-R@users.noreply.github.com>
2022-05-26 19:42:12 -0500

    more element names

--- miratope-core/src/lib.rs
+++ miratope-core/src/lib.rs
@@ -42,9 +42,10 @@ use abs::{
 use vec_like::VecLike;
 
 /// The names for 0-elements, 1-elements, 2-elements, and so on.
-const ELEMENT_NAMES: [&str; 12] = [
+const ELEMENT_NAMES: [&str; 25] = [
     "", "Vertices", "Edges", "Faces", "Cells", "Tera", "Peta", "Exa", "Zetta", "Yotta", "Xenna",
-    "Daka",
+    "Daka", "Henda", "Doka", "Tradaka", "Tedaka", "Pedaka", "Exdaka", "Zedaka", "Yodaka", "Nedaka",
+    "Ika", "Ikena", "Ikoda", "Iktra",
 ];
 
 /// The word "Components".
